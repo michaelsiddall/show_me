1a5480b7c5399ea41d29ccf633672761668e53f8
Mike Siddall <eatingartist@gmail.com>
2020-10-22 09:44:05 -0500

    added table info to sql database file, creating ShowDetails page

--- database.sql
+++ database.sql
@@ -3,8 +3,22 @@
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
-CREATE TABLE "user" (
+CREATE TABLE "user"
+(
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+
+CREATE TABLE "show"
+(
+    "id" SERIAL PRIMARY KEY,
+    "date" DATE NOT NULL,
+    "spotifyId" VARCHAR (250) NOT NULL,
+    "songKickId" INTEGER NOT NULL,
+    "review" VARCHAR (5000),
+    "user_id" INTEGER NOT NULL,
+    "favorite" BOOLEAN
+);
+
